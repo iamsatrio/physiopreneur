86d8da4a767febbb25445bff2c314f977c29c5db
Satrio Wibowo <satriowibowo@192.168.0.106>
2017-03-18 05:35:17 +0700

    mockup tambah pasien (android)

---
+++ android/PhysiopreneurMobile/app/src/main/java/com/physiopreneurmobile/DetailPasien.java
@@ -0,0 +1,13 @@
+package com.physiopreneurmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailPasien extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_pasien);
+    }
+}
--- android/PhysiopreneurMobile/app/src/main/java/com/physiopreneurmobile/PhysiotherapistHome.java
+++ android/PhysiopreneurMobile/app/src/main/java/com/physiopreneurmobile/PhysiotherapistHome.java
@@ -1,13 +1,61 @@
 package com.physiopreneurmobile;
 
+import android.content.Intent;
+import android.provider.ContactsContract;
+import android.renderscript.Allocation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class PhysiotherapistHome extends AppCompatActivity {
-
+    ImageView cariPasien, dataPasien, tambahPasien, pembayaran, buatLaporan, profileEmp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_physiotherapist_home);
+
+        cariPasien = (ImageView) findViewById(R.id.cari_pasien);
+        tambahPasien = (ImageView) findViewById(R.id.tambah_pasien);
+        dataPasien = (ImageView) findViewById(R.id.data_pasien);
+        pembayaran = (ImageView) findViewById(R.id.pembayaran);
+        buatLaporan = (ImageView) findViewById(R.id.buat_laporan);
+        profileEmp = (ImageView) findViewById(R.id.profile_emp);
+
+
+        cariPasien.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(PhysiotherapistHome.this, CariPasien.class);
+                startActivity(i);
+            }
+        });
+
+        tambahPasien.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(PhysiotherapistHome.this, TambahPasien.class);
+                startActivity(i);
+            }
+        });
+
+        dataPasien.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(PhysiotherapistHome.this, DataPasien.class);
+                startActivity(i);
+            }
+        });
+
+        pembayaran.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(PhysiotherapistHome.this, Pembayaran.class);
+                startActivity(i);
+            }
+        });
+
+
+
     }
 }
