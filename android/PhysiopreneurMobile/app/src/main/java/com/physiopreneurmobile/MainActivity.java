7cb106d3bf830d181cd93ea667d8af46ce419bc8
Satrio Wibowo <satriowibowo@192.168.0.106>
2017-03-18 02:11:22 +0700

    android login + home

--- android/PhysiopreneurMobile/app/src/main/java/com/physiopreneurmobile/MainActivity.java
+++ android/PhysiopreneurMobile/app/src/main/java/com/physiopreneurmobile/MainActivity.java
@@ -5,12 +5,14 @@ import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText pswd,usrusr;
-    TextView lin;
+    Button lin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        lin = (TextView) findViewById(R.id.lin);
+        lin = (Button) findViewById(R.id.lin);
         usrusr = (EditText) findViewById(R.id.usrusr);
         pswd = (EditText) findViewById(R.id.pswrdd);
 
@@ -27,15 +29,24 @@ public class MainActivity extends AppCompatActivity {
         lin.setTypeface(custom_font1);
         usrusr.setTypeface(custom_font);
         pswd.setTypeface(custom_font);
-//        sup.setOnClickListener(new View.OnClickListener()
-//        {
-//            @Override
-//            public void onClick(View v)
-//            {
-//                Intent it = new Intent(login.this, signup.class);
-//                startActivity(it);
-//            }
-//        });
 
+        lin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(usrusr.getText().toString().equals("")||pswd.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(), "mohon isi dengan benar", Toast.LENGTH_SHORT).show();
+                }else {
+                    if (usrusr.getText().toString().equals("manager") && pswd.getText().toString().equals("manager")) {
+                        Intent it = new Intent(MainActivity.this, ManagerHome.class);
+                        startActivity(it);
+//                    Toast.makeText(getApplicationContext(), pswd.getText().toString(),
+//                            Toast.LENGTH_LONG).show();
+                    } else if(usrusr.getText().toString().equals("emp")||pswd.getText().toString().equals("emp")){
+                        Intent it = new Intent(MainActivity.this, PhysiotherapistHome.class);
+                        startActivity(it);
+                    }
+                }
+            }
+        });
     }
 }
---
+++ android/PhysiopreneurMobile/app/src/main/java/com/physiopreneurmobile/PhysiotherapistHome.java
@@ -0,0 +1,13 @@
+package com.physiopreneurmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhysiotherapistHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_physiotherapist_home);
+    }
+}
