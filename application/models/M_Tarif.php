2a85a5345edc230adc74585dd489b3169a812d2e
findco_team <findcoteam@gmail.com>
2017-05-05 10:13:32 +0700

    revise model tarif

--- application/models/M_Tarif.php
+++ application/models/M_Tarif.php
@@ -1,34 +1,37 @@
 <?php
-class M_Tarif extends CI_Model{
-
-    function __construct(){
+class M_Tarif extends CI_Model
+{
+    public function __construct()
+    {
         parent::__construct();
     }
 
-  	function tampil_tarif()
-  	{
-  		$this->db->select("*");
-  		$this->db->from('tb_tarif a');
-  		$this->db->join("tb_jenis_pembayaran b","a.id_jenis_pembayaran = b.id");
-      $this->db->join("tb_lokasi c","a.id_lokasi = c.id");
-  		return $this->db->get();
-  	}
+    public function tampil_tarif()
+    {
+        $this->db->select("*");
+        $this->db->from('tb_tarif a');
+        $this->db->join("tb_jenis_pembayaran b", "a.id_jenis_pembayaran = b.id");
+        $this->db->join("tb_lokasi c", "a.id_lokasi = c.id");
+        $this->db->order_by("c.id");
+        return $this->db->get();
+    }
 
-  	function allLokasi(){
-  		$this->db->select("*");
-  		$this->db->from("tb_lokasi");
-  		return $this->db->get();
-  	}
+    public function allLokasi()
+    {
+        $this->db->select("*");
+        $this->db->from("tb_lokasi");
+        return $this->db->get();
+    }
 
-    function allJenisPembayaran(){
-  		$this->db->select("*");
-  		$this->db->from("tb_jenis_pembayaran");
-  		return $this->db->get();
-  	}
-	
-	function tambahTarif($data,$table){
-		$this->db->insert($table,$data);
-	}
+    public function allJenisPembayaran()
+    {
+        $this->db->select("*");
+        $this->db->from("tb_jenis_pembayaran");
+        return $this->db->get();
+    }
 
+    public function tambahTarif($data, $table)
+    {
+        $this->db->insert($table, $data);
+    }
 }
-?>
